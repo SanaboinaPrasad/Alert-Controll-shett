1553ba08b7a908c8024ca0ee3cee1525c2211035
Sriram Prasad <sriramprasad@MacBook-Pro.local>
2018-11-28 10:08:23 +0530

    Commit

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let alertsheetButton :UIButton = {
+       let button  = UIButton(type: .system)
+            button.backgroundColor = .red
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.setTitle("Knock Knock", for: .normal)
+        button.addTarget(self, action: #selector(alertSheetShow), for: .touchUpInside)
+        return button
+    }()
+    let alertbutton :UIButton = {
+        let button  = UIButton(type: .system)
+        button.backgroundColor = .red
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Knock Knock", for: .normal)
+        button.addTarget(self, action: #selector(alertShow), for: .touchUpInside)
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+            view.addSubview(alertsheetButton)
+            view.addSubview(alertbutton)
+            addConstraints()
+    }
+    @objc func alertSheetShow(){
+        
+        let alert = UIAlertController(title: "Knock Knock", message: "Who is he", preferredStyle: .actionSheet)
+        let action = UIAlertAction(title: "OK", style: .default) { (_) in
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
+    @objc func alertShow(){
+        let alert = UIAlertController(title: "Alet", message: "See the Message", preferredStyle: .alert)
+        let action = UIAlertAction(title: "alet showing", style: .default) { (_) in
+            
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    func addConstraints(){
+    let constraints  = [
+    
+        alertsheetButton.topAnchor.constraint(equalTo: view.topAnchor,constant: 100),
+        alertsheetButton.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 100),
+        alertsheetButton.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -100),
+        alertsheetButton.heightAnchor.constraint(equalToConstant: 100),
+        
+        alertbutton.topAnchor.constraint(equalTo: alertsheetButton.bottomAnchor,constant: 100),
+        alertbutton.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 100),
+        alertbutton.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -100),
+        alertbutton.heightAnchor.constraint(equalToConstant: 100),
 
+        
+    ]
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
